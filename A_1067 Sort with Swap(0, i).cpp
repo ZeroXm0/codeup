70969e8486a1ef71c05a3b7080461215be8239a0
morningM0 <zeroindexweb@gmail.com>
2019-07-11 17:29:01 +0800

    Update A_1067 Sort with Swap(0, i).cpp

--- A_1067 Sort with Swap(0, i).cpp
+++ A_1067 Sort with Swap(0, i).cpp
@@ -5,31 +5,32 @@ const int maxn = 100010;
 int pos[maxn];
 
 int main() {
-	int n, num,ans = 0;//num 标记0所在位子 
+	int n, num,ans = 0;//num 标记自己所在位子 
 	scanf("%d", &n);
+	int left = n - 1;//表示不再最终位子上的数字个数（除0外） 
 	for(int i = 0; i < n; i++) {
-		scanf("%d", &pos[i]);
-		if(pos[i] == 0) num = i;
+		scanf("%d", &num);
+		pos[num] = i;
+		if(num == i && num != 0) left--;
 	}
-	int k = 0;//记录未归为的最小数 
-	while(k < n){
-		if(num != 0){
-			int m;//记录i的位子 
-			for(int i = 0; i < n; i++) {
-				if(pos[i] = num) m = i;
+	int k = 1;//除0外不在最终位子上的最小数字 
+	while(left > 0) {
+		if(pos[0] == 0){//如果0在0位上，与一个不在最终位子上的数交换 
+			while(k < n) {
+				if(pos[k] != k){
+					swap(pos[0], pos[k]);
+					ans++;
+					break;
+				}
+				k++;
 			}
-			swap(pos[num], pos[m]);
-			num = m;
-		} else {
-			for(int i = 0; i < n; i++) {
-				if(pos[i] != 0 && pos[i] != i) k = i;
-			}
-			swap(pos[num], pos[k]);
-			num = k;
 		}
-		ans++;
-	} 
+		while(pos[0] != 0) {//0不在0位上，就与其所在位子的数字交换 
+			swap(pos[0], pos[pos[0]]);
+			ans++;
+			left--;
+		}
+	}
 	printf("%d\n", ans);
 	return 0;
-	
 }
